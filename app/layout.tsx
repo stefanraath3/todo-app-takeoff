1f75e62037b3c971e805a571d227f79535c4455b
Stefan Raath <stefanraath3@gmail.com>
2024-09-07 15:18:44 +0200

    add layout code and theme switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import { Toaster } from "@/components/ui/toaster";
 import { Providers } from "@/components/utilities/providers";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -7,14 +9,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Todo App",
-  description: "A simple todo app.",
+  description: "A full-stack template for a todo app.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -24,6 +26,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </Providers>
       </body>
     </html>
